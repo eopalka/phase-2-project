1933a6d311c01db7903d53f6c1a384d99d0b21a0
elisa <elisaopalka@gmail.com>
2021-01-05 20:04:52 -0500

    set up path for sign up and log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,21 +8,46 @@ class UsersController < ApplicationController
     #recieve the data (params) from the login form
     post "/login" do 
     #find the user
-    user = User.find_by(email: params[:email])
+    @user = User.find_by(email: params[:email])
         #authenticate the user
-        if user && user.authenticate(params[:password])
+        if @user && @user.authenticate(params[:password])
             #creating a key/value pair in the session hash using the users id to actually log them in
             session[:user_id] = user.id
+            flash[:message] = "Nice to see ya #{@user.name}!"
             #redirect to user's profile
-            redirect "/users/#{user.id}"
+            redirect "/users/#{@user.id}"
         else
-            #show error message
+            flash[:errors] = "Please try again!"
             redirect '/login'
         end
     end
 
-    #user SHOW route
     get "/users/:id" do
-        "users show page!"
+        @user = User.find_by(id: params[:id])
+        erb :'/users/show'
     end
+
+    get '/signup' do
+        erb :signup
+    end
+
+    post '/users' do
+        @user = User.new(params)
+        if @user.save
+          session[:user_id] = @user.id
+          flash[:message] = "#{@user.name}, Welcome!"
+          redirect "/users/#{@user.id}"
+        else
+          #not valid input
+          flash[:errors] = "Well that did not work because #{@user.errors.full_messages.to_sentence}"
+          redirect '/signup'
+        end
+      end
+        
+    get '/logout' do
+        session.clear
+        redirect '/'
+    end
+
+
 end
