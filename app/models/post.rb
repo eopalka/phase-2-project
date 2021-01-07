b74e5e7858c793a4fa16169ab26e1f16e509eecb
elisa <elisaopalka@gmail.com>
2021-01-06 19:34:17 -0500

    validates attributes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
     belongs_to :user
+
+    validates :title, presence: true
+    validates :author, presence: true
+    validates :copyright_date, presence: true
+    validates :image_url, presence: true
+    validates :description, presence: true
+    
 end
