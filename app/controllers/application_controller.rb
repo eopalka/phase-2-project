82c3742838f44bb721ac53434e51dc549b926f3b
elisa <elisaopalka@gmail.com>
2021-01-06 19:28:31 -0500

    authorize to edit route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,33 +20,28 @@ class ApplicationController < Sinatra::Base
   end
 
 
-  get "/" do
-    if logged_in?
-      redirect "/users/#{current_user.id}"
-    else
-      erb :welcome
+  helpers do
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
     end
+
+    #returns a boolean
+    def logged_in?
+      #current_user will return nil or the entire user object (we want true/false)
+      !!current_user
+    end
+
+    def redirect_if_not_logged_in
+      if !logged_in?
+        redirect '/login'
+      end
+    end
+
+    def authorized_to_edit?(post)
+      post.user == current_user
+    end
+
   end
 
-  helpers do
-  #   # returns boolean if user is logged in or not - double bang converts value
-    def logged_in?
-        !!current_user
-    end
-    #keeps track of the logged in user
-    def current_user
-      User.find_by(id: session[:user_id])
-    end
-  #   # #authorization helper for editing & deleting posts
-  #   # def authorized_to_edit?(post)
-  #   #   post.user == current_user
-  #   # end
-  # #   def redirect_if_not_logged_in
-  # #     if !logged_in?
-  # #       flash[:error] = "You must be logged in to view this page"
-  # #       redirect '/login'
-  # #     end
-  # # end
-  end
 
 end
