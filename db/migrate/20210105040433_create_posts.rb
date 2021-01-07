38697c69ba1acf2388ef07618006c4acedb0a209
elisa <elisaopalka@gmail.com>
2021-01-06 19:19:14 -0500

    tried adding author and coppyright data to table

--- db/migrate/20210105040433_create_posts.rb
+++ db/migrate/20210105040433_create_posts.rb
@@ -2,6 +2,8 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :title
+      t.string :author
+      t.integer :copyright_date
       t.string :image_url
       t.text :description
       t.integer :user_id
