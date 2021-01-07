3a15588485e6a00d8d910c2315e94cc41b12e1e8
elisa <elisaopalka@gmail.com>
2021-01-06 19:17:53 -0500

    seeded a post to test with

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 #Here is where I will write the seed data for my program
 
-Elisa = User.create(email: "seesawsesa@yahoo.com", name: "Elisa", password: "hello")
+elisa = User.create(email: "seesawsesa@yahoo.com", name: "Elisa", password: "hello")
+
+Post.create(title: "“Harry Potter and the Deathly Hallows” (2007)", image_url: "https://upload.wikimedia.org/wikipedia/en/a/a9/Harry_Potter_and_the_Deathly_Hallows.jpg", description: "This is my favorite Harry Potter book. It was about time they got their shit together and started getting serious about fighting Voldemort. I mean seriously how many of you does he need to attack before you do something about it.", user_id: elisa.id)
