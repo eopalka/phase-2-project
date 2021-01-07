185729287678203fcfe361191532f2238e3492e5
elisa <elisaopalka@gmail.com>
2021-01-06 21:47:19 -0500

    commented out validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
     has_secure_password
 
-    validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
+    # validates :name, presence: true
+    # validates :email, presence: true, uniqueness: true
 
     has_many :posts
 end
