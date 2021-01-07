64c04986d111e12892a5b74d8ecfbb66fe26ada5
elisa <elisaopalka@gmail.com>
2021-01-06 21:46:28 -0500

    drop table to add author and publish date

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,13 @@ ActiveRecord::Schema.define(version: 20210105040433) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
+    t.string   "author"
+    t.integer  "copyright_date"
     t.string   "image_url"
     t.text     "description"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "users", force: :cascade do |t|
