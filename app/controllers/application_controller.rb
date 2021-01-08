f3d37b51db47c8212d23d56aed851655f722eade
elisa <elisaopalka@gmail.com>
2021-01-08 18:00:37 -0500

    added if statement

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
     #returns a boolean
